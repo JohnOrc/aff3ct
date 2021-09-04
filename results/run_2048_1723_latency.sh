268cbda1d681be215c37ec744a4c342f8361c047
{[(He110wor1d)]} <117john.orc@gmail.com>
2021-09-04 21:54:27 +0800

    add latency sh

--- results/run_2048_1723_latency.sh
+++ results/run_2048_1723_latency.sh
@@ -7,60 +7,118 @@ output_dir="./SCL/"
 n="2048"
 k="1723"
 crc_type="32-GZIP"
-nodes="{R0,R0L,R1,REP,REPL,SPC4+}"
-sim_num="10000"
-
+num_thread="1"
+seed="42"
 
 method0="FAST"
-echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method
-
-list_size="8"
-output_file=$USER"_"$n"_"$k"_"$list_size"_"$crc_type"_"$method"_"$nodes".txt"
-
-echo "L="$list_size
-$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4.25" -M "4.3" -s "0.25" -S 42 -t 1 -n $sim_num --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method0 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes -L $list_size >> $output_dir$output_file
-
-list_size="16"
-output_file=$USER"_"$n"_"$k"_"$list_size"_"$crc_type"_"$method"_"$nodes".txt"
-
-echo "L="$list_size
-$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4.25" -M "4.3" -s "0.25" -S 42 -t 1 -n $sim_num --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method0 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes -L $list_size >> $output_dir$output_file
-
-list_size="32"
-output_file=$USER"_"$n"_"$k"_"$list_size"_"$crc_type"_"$method"_"$nodes".txt"
-
-echo "L="$list_size
-$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4.25" -M "4.3" -s "0.25" -S 42 -t 1 -n $sim_num --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method0 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes -L $list_size >> $output_dir$output_file
-
-list_size="64"
-output_file=$USER"_"$n"_"$k"_"$list_size"_"$crc_type"_"$method"_"$nodes".txt"
-
-echo "L="$list_size
-$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4.25" -M "4.3" -s "0.25" -S 42 -t 1 -n $sim_num --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method0 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes -L $list_size >> $output_dir$output_file
-
 method1="FASTCS"
-echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method
+method2="FASTCS1"
+method3="FASTCS2"
+
+nodes4p="{R0,R0L,R1,REP,REPL,SPC_4+}"
+
+
+
+list_size="4"
+echo "L="$list_size
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method0
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method0"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method0 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method1
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method1"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method1 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method2
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method2"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method2 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method3
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method3"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method3 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
 
 list_size="8"
-output_file=$USER"_"$n"_"$k"_"$list_size"_"$crc_type"_"$method"_"$nodes".txt"
-
 echo "L="$list_size
-$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4.25" -M "4.3" -s "0.25" -S 42 -t 1 -n $sim_num --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method1 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes -L $list_size >> $output_dir$output_file
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method0
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method0"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method0 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method1
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method1"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method1 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method2
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method2"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method2 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method3
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method3"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method3 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
 
 list_size="16"
-output_file=$USER"_"$n"_"$k"_"$list_size"_"$crc_type"_"$method"_"$nodes".txt"
-
 echo "L="$list_size
-$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4.25" -M "4.3" -s "0.25" -S 42 -t 1 -n $sim_num --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method1 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes -L $list_size >> $output_dir$output_file
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method0
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method0"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method0 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method1
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method1"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method1 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method2
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method2"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method2 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method3
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method3"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method3 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
 
 list_size="32"
-output_file=$USER"_"$n"_"$k"_"$list_size"_"$crc_type"_"$method"_"$nodes".txt"
-
 echo "L="$list_size
-$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4.25" -M "4.3" -s "0.25" -S 42 -t 1 -n $sim_num --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method1 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes -L $list_size >> $output_dir$output_file
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method0
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method0"_"$nodes4p".txt"
 
-list_size="64"
-output_file=$USER"_"$n"_"$k"_"$list_size"_"$crc_type"_"$method"_"$nodes".txt"
 
-echo "L="$list_size
-$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4.25" -M "4.3" -s "0.25" -S 42 -t 1 -n $sim_num --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method1 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes -L $list_size >> $output_dir$output_file
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method0 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method1
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method1"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method1 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method2
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method2"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method2 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
+
+echo "processing: n = "$n" k = "$k" crc_type = "$crc_type" method = "$method3
+output_file=$USER"_LATEN_"$n"_"$k"_"$list_size"_"$crc_type"_"$method3"_"$nodes4p".txt"
+
+
+$target_dir$target_file -p "32" --sim-type "BFER" -C "POLAR" -m "4" -M "4" -s "0.25" -t 1 -e "10" --sim-seed $seed --sim-stats --sim-meta $output_file --enc-fb-gen-method "GA" -K $k -N $n --src-type "RAND"  --chn-type "AWGN"  --dec-type "SCL" --dec-implem $method3 --dec-simd "INTRA" --crc-type $crc_type --crc-implem "FAST" --dec-polar-nodes $nodes4p -L $list_size >> $output_dir$output_file
